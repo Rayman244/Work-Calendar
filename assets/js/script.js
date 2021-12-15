f82e76c4b48853f652f0bf717a1f035ac2f48a1c
MyMain <rayhewitt42@icloud.com>
2021-12-15 16:59:14 -0500

    finished layout and highlighting the current time red

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
 
 var fullDate = moment().format('dddd MMMM Do YYYY')
 var curdayTxt = $('#currentDay')
+var curHour = moment().format('h')
 curdayTxt.text(fullDate)
-console.log(curdayTxt);
+console.log(curHour);
+var timeFrame = $('.culTime')
+var time2 = $('.culTime[data-time]')
+
+console.log(time2);
+
+
+console.log(timeFrame);
+for (let i = 0; i < timeFrame.length - 1; i++) {
+    const element = timeFrame.attr('data-time');
+    console.log(element);
+
+}
+
+
+
+$( `.culTime[data-time= ${curHour}]` ).parent().addClass('bg-danger');
